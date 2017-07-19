55c63c7f3a4695bbc8d4feae647f736082c62586
shenna <akirashirase90@gmail.com>
2017-07-19 12:39:56 +0300

    Template / CSS changes

--- kb/config.js
+++ kb/config.js
@@ -4,7 +4,7 @@
 var config = {
 
   // Your site title (format: page_title - site_title)
-  site_title: 'Elysium Project',
+  site_title: 'Elysium Project Support',
 
   // The base URL of your site (can use %base_url% in Markdown files)
   base_url: '',
